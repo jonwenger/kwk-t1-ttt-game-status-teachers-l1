0b641a5826886d4599cfabf5916c19bba36d0a23
Jon Wenger <jonwenger783@gmail.com>
2018-05-19 21:50:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,11 +5,11 @@ end
 
 # Define your WIN_COMBINATIONS constant
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-puts board
-def won?(board)
-WIN_COMBINATIONS(board)
-end
 
 top_row_win = [0,1,2]
 mid_row_win = [3,4,5]
 bottom_row_win = [6,7,8]
+
+WIN_COMBINATIONS = [top_row_win, mid_row_win, bottom_row_win]
+
+puts WIN_COMBINATIONS
